264fdeabee16ea064c540c97e9d4aca8ad344d1a
William <wisofer17@gmail.com>
2026-01-06 10:04:36 -0600

    centrando el texto

--- obj/Debug/net9.0/BarberPro.AssemblyInfo.cs
+++ obj/Debug/net9.0/BarberPro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BarberPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51f62cb02427220efddf43e74b85b79def8aa9b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34c21bfee9ead02bed62a86c422e548ed2eb6335")]
 [assembly: System.Reflection.AssemblyProductAttribute("BarberPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BarberPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
